c02862715e758c63e7bcfda9eb80cc407316b053
gyorgy-s <140075614+gyorgy-s@users.noreply.github.com>
2023-12-06 06:55:24 +0100

    feat: add response and error handling

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, request, jsonify, make_response
 
 from . import control
 
@@ -12,14 +12,58 @@ def home():
 
 @routes.route("/get-posts")
 def get_posts():
-    posts = control.get_posts()
-    return jsonify(posts)
+    num = 0
+    page = 1
+    try:
+        if request.args.get("num"):
+            num = int(request.args.get("num"))
+        if request.args.get("page"):
+            page = int(request.args.get("page"))
+    except ValueError:
+        response = make_response({
+            "error": "Value given for 'num' or 'page' is not accepted."
+        }, 400)
+    else:
+        posts = control.get_posts(num=num, page=page)
+        if not posts:
+            response = make_response({
+                "error": [
+                    f"There are no post in the range of num:{num}, page: {page}."
+                ]
+            }, 404)
+        else:
+            response = make_response(jsonify(posts), 200)
+    return response
 
 
-@routes.route("/get-post/<int:id>")
-def get_post(id):
-    post = control.get_post(id)
-    return jsonify(post)
+@routes.route("/get-post")
+def get_post():
+    if not request.args.get("id"):
+        response = make_response({
+            "error":[
+                "Missing parameter 'id'."
+            ]
+        }, 400)
+    else:
+        try:
+            id = int(request.args.get("id"))
+        except ValueError:
+            make_response({
+                "error":[
+                    "Value given for 'id' is not accepted."
+                ]
+            }, 400)
+        else:
+            post = control.get_post(id)
+            if not post:
+                response = make_response({
+                    "error":[
+                        f"There is no post with the 'id': {id}."
+                    ]
+                }, 404)
+            else:
+                response = make_response(jsonify(post), 200)
+    return response
 
 
 @routes.route("/get-posts-by-user")
